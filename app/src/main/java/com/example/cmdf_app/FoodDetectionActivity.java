23e25bd9ea7336b834294b910d242ff48e686d9e
andrea <andreamtang@gmail.com>
2019-03-10 01:50:29 -0800

    altered project structure

--- app/src/main/java/com/example/cmdf_app/FoodDetectionActivity.java
+++ app/src/main/java/com/example/cmdf_app/FoodDetectionActivity.java
@@ -1,6 +1,18 @@
 package com.example.cmdf_app;
 
+
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.google.api.services.vision.v1.Vision;
 
 public class FoodDetectionActivity extends AppCompatActivity {
+
+    private Vision vision;
+    ImageView imageView;
+
+//    protected void onCreate(Bundle savedInstanceState) {
+//    }
+
 }
--- app/src/main/java/com/example/cmdf_app/Nutrition.java
+++
@@ -1,8 +0,0 @@
-package com.example.cmdf_app;
-
-public class Nutrition {
-    /**
-     * parse info from Nutrition API
-     */
-
-}
